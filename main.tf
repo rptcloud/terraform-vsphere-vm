64ac2c7d7f67402e567df804d43729b7737d7979
River Point Technology <47562359+rptdata@users.noreply.github.com>
2020-10-02 08:50:56 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,57 @@
+provider "vsphere" {
+  # If you have a self-signed cert
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "dc" {
+  name = "Datacenter"
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = "raid10_gen7"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_resource_pool" "pool" {
+  name          = "Resources"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+  name          = "VM Network"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = "terraform-test"
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id     = data.vsphere_datastore.datastore.id
+
+  num_cpus = 2
+  memory   = 1024
+  guest_id = "other3xLinux64Guest"
+
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+
+  disk {
+    label = "disk0"
+    size  = 20
+  }
+
+   provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "root"
+      password = "P@ssw0rd01"
+      host     = vsphere_virtual_machine.vm.default_ip_address
+    }
+
+    inline = [
+      "yum -y update"
+    ]
+
+  }
+
+}
